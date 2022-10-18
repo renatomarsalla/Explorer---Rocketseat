bd587fb88863d629eb04463a4cd609b5748e604b
Renato Marsalla <56097330+renatomarsalla@users.noreply.github.com>
2022-10-18 20:47:27 -0300

    Delete 09-ReactJS/rocketnotes directory

--- 09-ReactJS/rocketnotes/src/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from './styles.js';
-
-export function Button({ title, loading = false, width, ...rest }) {
-  return (
-    <Container type="button" disabled={loading} {...rest}>
-      {loading ? 'Carregando...' : title}
-    </Container>
-  );
-}
--- 09-ReactJS/rocketnotes/src/components/ButtonText/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from './styles.js';
-
-export function ButtonText({ title, isActive = false, ...rest }) {
-  return (
-    <Container type="button" isActive={isActive} {...rest}>
-      {title}
-    </Container>
-  );
-}
